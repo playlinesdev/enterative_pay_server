7f49e5acfcdc80baee96f5c79d01838bc04bac8d
rafante2@gmail.com <rafante2@gmail.com>
2020-11-15 20:38:01 -0300

    set dynamic port for server execution

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+var port = process.env.ENTERATIVE_SERVER_PORT ?? 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Running on port ${port}`);
 }
 bootstrap();
